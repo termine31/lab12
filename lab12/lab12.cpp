9a95a38cf6d08f3e7ab8076bae5c021142052135
termine1 <gsafdq.tn.qe12@gmail.com>
2026-03-22 15:03:28 +0700

    2 СПОСОБ ЧЕРЕЗ АЛГОРИТМ Создаю вектора(списки) для удобства копирую функцию с 1 способа функция расстановки: 1 условие, если все фигуры расставлены, то счетчик +1 и выводим на доску
    2 проверка
    циклом проверяем от 1 до i столбца
    Вместо 3 проверок, как в прошлом способе, в одном условии сразу пишем эти 2 условия
    1 условие - что в столбец i нет ферзей. (==0 значит стобец свободен)
    2 условие - проверка правой диагонали
    3 условие - проверка левой диагонали
    Если условие выполняется то:
    1 занимаем столбец
    2 занимаем правую диагональ
    3 занимаем левую диагональ
    4 запоминаем позицию ферзя
    Далее переход к следующей строке
    После, идет бэктрекинг
    Исследовали все вариант с этой позицией - убираем его и пробуем другие варианты
    
    в основной программе:
    заполняем массивы 0 и вызываем функцию + вывод результатов

--- lab12/lab12.cpp
+++ lab12/lab12.cpp
@@ -1,6 +1,7 @@
 ﻿
 #include <iostream>
 #include <cmath>
+#include <vector>
 using namespace std;
 
 int scount = 0;
@@ -34,6 +35,7 @@ bool pos(int** mat, int str1, int stol) {
 }
 // вывод доски
 void print1(int** mat) {
+    cout << "----------------------------------------------------------------" << endl;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             if (mat[i][j] == 1)
@@ -62,20 +64,70 @@ void passtonovka(int str1, int** mat) {
         }
     }
 }
+//====================================================================================================================================
+// 2 СПОСОБ 
+int scount2 = 0;
+int N2;
 
+vector<int> S;  
+vector<int> R;  
+vector<int> L;  
+vector<int> Q;  
 
+// вывод
+void print2() {
+    cout << "----------------------------------------------------------------" << endl;
+    for (int i = 1; i <= N2; i++) {
+        for (int j = 1; j <= N2; j++) {
+            if (Q[i] == j)
+                cout << "Q ";
+            else
+                cout << ". ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
 
+void passtanovka2(int j) {  // j — тек строка
+    if (j > N2) {
+        scount2++;
+        print2();
+        return;
+    }
+
+    for (int i = 1; i <= N2; i++) {  // i — столбец
+        if (S[i] == 0 && R[j - i + N2] == 0 && L[j + i] == 0) {
+            S[i] = 1;
+            R[j - i + N2] = 1;
+            L[j + i] = 1;       // диагонали через j i - заняты
+            Q[j] = i;
+
+            passtanovka2(j + 1);
+
+            S[i] = 0; 
+            R[j - i + N2] = 0;   // бэк
+            L[j + i] = 0;
+        }
+    }
+}
 
 
 
 int main()
 {
  // Метод матрицей
+    cout << "1 СПОСОБ(матрица) " << endl;
     cout << "Введите кол-во ферзей(n): ";
     cin >> N;
 
+    if (N <= 0) {
+        cout << "N должно быть больше 0 ";
+        return 1;
+    }
+
     int** mat = new int* [N];
-    for (int i = 0; i < N; i == 0) {
+    for (int i = 0; i < N; i++) {
         mat[i] = new int[N];
         for (int j = 0; j < N; j++) {
             mat[i][j] = 0; 
@@ -84,12 +136,38 @@ int main()
 
     scount = 0;
     passtonovka(0, mat);
-    cout << "Решений для " << N << "ферзей: " << scount << endl;
+    cout << "Решений для " << N << " ферзей: " << scount << endl;
 
     for (int i = 0; i < N; i++) {
         delete[] mat[i];
     }
     delete[] mat;
 
+
+    cout << "==========================================================" << endl;
+    cout << "2 СПОСОБ(АЛГОРИТМ)" << endl;
+    //==========================================================
+    // 2 способ 
+
+
+    cout << "Введите кол-во ферзей (оптимизированный способ): ";
+    cin >> N2;
+
+    if (N2 <= 0) {
+        cout << "N должно быть больше 0" << endl;
+        return 1;
+    }
+
+    S.resize(N2 + 1, 0);
+    Q.resize(N2 + 1, 0);
+    R.resize(2 * N2 + 5, 0);   // заполнение массивов 0 
+    L.resize(2 * N2 + 5, 0);
+
+    scount2 = 0;
+    passtanovka2(1);
+
+    cout << "Решений для " << N2 << " ферзей: " << scount2 << endl;
+
     return 0;
+
 }
