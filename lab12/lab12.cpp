c6ee6db818fd1192c593044dd64436b08d2260db
termine1 <gsafdq.tn.qe12@gmail.com>
2026-03-22 13:53:17 +0700

    Расстановка ферзей(любимый бэктрекинг) Первое условие Условие, что все ферзи расставлены в строках от 0 до n-1. Если выполняется, то выводим доску Цикл пытается поставить ферзя в строку, в все возможные столбцы Ставим 1 - то что клетка занята Рекурсивный вызов: переход на другую строку (str1 +1) и пытаемся расставить оставшихся ферзей Ключевой момент!!! конечная строка: после того как мы исследовали все вариант ферзя в этой позиции, мы его убираем и пробуем другой вариант

--- lab12/lab12.cpp
+++ lab12/lab12.cpp
@@ -32,8 +32,8 @@ bool pos(int** mat, int str1, int stol) {
 
     return true;
 }
-
-void printBoard(int** mat) {
+// вывод доски
+void print1(int** mat) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             if (mat[i][j] == 1)
@@ -46,6 +46,22 @@ void printBoard(int** mat) {
     cout << endl;
 }
 
+// расстановка
+void passtonovka(int str1, int** mat) {
+    if (str1 == N) {  // Все ферзи расставлены
+        scount++; // счетчик решений
+        print1(mat);
+        return;
+    }
+
+    for (int col = 0; col < N; col++) {
+        if (pos(mat, str1, col)) {
+            mat[str1][col] = 1;          
+            passtonovka(str1 + 1, mat);        
+            mat[str1][col] = 0;          
+        }
+    }
+}
 
 
 
