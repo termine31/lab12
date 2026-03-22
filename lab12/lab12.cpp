a16cb55f3ec08a7b44656dbfd39333e87843d734
termine1 <gsafdq.tn.qe12@gmail.com>
2026-03-22 13:13:10 +0700

    СПОСОБ МАТРИЦЕЙ Создаю функцию для проверки 3 направлений, куда ферзь может попасть 1) Вертикально(в) берем какую то позицию и проверяем: по циклу проходимся от 0 до строки, которую проверяем(ее не включаем). и смотрим чтоб позиция была == 0, если равна 1, то там стоит ферзь <=> поставить ферзя в позицию которую проверяем нельзя! 2) Проверка главной диагонали i-- и j-- - двигаемся вверх-влево Идем пока не выйдем за пределы доски Также проверяем клетку, куда вступаем на наличие ферзя 3) Побочная диагональ Идем вверх-вправо i-- вверх строка, а j++ вправо столбец также идем до конца строки Проверка на наличие ферзя

--- lab12/lab12.cpp
+++ lab12/lab12.cpp
@@ -1,9 +1,54 @@
-﻿// lab12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
 #include <iostream>
+#include <cmath>
+using namespace std;
+
+int scount = 0;
+int N;
+
+// СПОСОБ 1 МАТРИЦЫ
+// Проверка
+bool pos(int** board, int str1, int stol) {
+    // Пр. по столбцу 
+    for (int i = 0; i < str1; i++) {
+        if (board[i][stol] == 1) {
+            return false;
+        }
+    }
+
+    // Пр. главной диагонали (вверх-влево)
+    for (int i = str1, j = stol; i >= 0 && j >= 0; i--, j--) {
+        if (board[i][j] == 1) {
+            return false;
+        }
+    }
+
+    // Пр. побочной диагонали (вверх-вправо)
+    for (int i = str1, j = stol; i >= 0 && j < N; i--, j++) {
+        if (board[i][j] == 1) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+
+
+
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+ 
+
+
+
+
+
+
+
 }
