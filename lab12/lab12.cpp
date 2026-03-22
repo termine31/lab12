c33f547a5da0c9a34b2575e48d5f06d3c2fa3a28
termine1 <gsafdq.tn.qe12@gmail.com>
2026-03-22 13:25:26 +0700

    ФУНКЦИЯ ВЫВОДА ДОСКИ 2 цикла - перебор по  каждой строке и столбцу, если позиция == 1, то ставим Q - это ферзь, если позиция свободна, тоесть 0, то ставим .

--- lab12/lab12.cpp
+++ lab12/lab12.cpp
@@ -8,24 +8,24 @@ int N;
 
 // СПОСОБ 1 МАТРИЦЫ
 // Проверка
-bool pos(int** board, int str1, int stol) {
+bool pos(int** mat, int str1, int stol) {
     // Пр. по столбцу 
     for (int i = 0; i < str1; i++) {
-        if (board[i][stol] == 1) {
+        if (mat[i][stol] == 1) {
             return false;
         }
     }
 
     // Пр. главной диагонали (вверх-влево)
     for (int i = str1, j = stol; i >= 0 && j >= 0; i--, j--) {
-        if (board[i][j] == 1) {
+        if (mat[i][j] == 1) {
             return false;
         }
     }
 
     // Пр. побочной диагонали (вверх-вправо)
     for (int i = str1, j = stol; i >= 0 && j < N; i--, j++) {
-        if (board[i][j] == 1) {
+        if (mat[i][j] == 1) {
             return false;
         }
     }
@@ -33,7 +33,18 @@ bool pos(int** board, int str1, int stol) {
     return true;
 }
 
-
+void printBoard(int** mat) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (mat[i][j] == 1)
+                cout << "Q ";
+            else
+                cout << ". ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
 
 
 
